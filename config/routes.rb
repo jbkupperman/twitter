1e37e9591c7ab7afe2c68f54556676104c8aaebe
Jake Kupperman <jbkupperman@gmail.com>
2016-03-07 20:53:21 -0600

    Create Tweets controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
+
+  get 'tweets/show'
+
+  get 'tweets/edit'
+
+  get 'tweets/new'
+
+  get 'tweets/_form'
+
   devise_for :users
+
+  resources :tweets
+
+  root "tweets#index"
+
   devise_for :installs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
