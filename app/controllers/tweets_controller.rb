cc0e17faca15b93619d364510765cb7a01cf4fbc
Jake Kupperman <jbkupperman@gmail.com>
2016-03-13 20:11:36 -0500

    Format Tweets controller and views

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,14 @@
 class TweetsController < ApplicationController
+
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
+
+
+  # actions (and other controller code) below
+  end
+
   def index
+    @tweets = Tweet.all
   end
 
   def show
@@ -9,8 +18,52 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+  respond_to do |format|
+    if @tweet.update(tweet_params)
+      format.html { redirect_to @tweet, notice: 'Post was successfully updated.' }
+    else
+      format.html { render :edit }
+    end
+   end
+  end
+
+  def destroy
+  @tweet.destroy
+
+  respond_to do |format|
+    format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
+   end
   end
 
   def _form
   end
+
+private
+
+ def set_tweet
+  @tweet = Tweet.find(params[:id])
+ end
+
+ def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
+ end
+
 end
+
+
